14a90f4c3f3104c7257c6140918b4963e9f92ee0
morphthesworph <morphsworph@gmail.com>
2025-03-20 15:24:46 -0400

    added transition to building
    skibidi bop hmm dada

--- reactor_building_front.gd
+++ reactor_building_front.gd
@@ -1,7 +1,4 @@
 extends Node2D
-func _input(event: InputEvent) -> void:
-	$PlayerBody.velocity=Vector2(event.get_action_strength("ui_right")-event.get_action_strength("ui_left"),event.get_action_strength("ui_down")-event.get_action_strength("ui_up"))
-	if ($PlayerBody.velocity.x**2+$PlayerBody.velocity.y**2)**.5 !=0:
-		$PlayerBody.velocity/=($PlayerBody.velocity.x**2+$PlayerBody.velocity.y**2)**.5
-		$PlayerBody.velocity*=10
-		$PlayerBody.move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://building.tscn")
